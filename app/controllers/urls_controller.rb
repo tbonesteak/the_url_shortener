0c34f3d691113e2d2d3ec7f4350b43faa7358875
Apprentice <apprentice@devbootcamp.com>
2012-11-15 20:38:24 -0800

    Worked on controller and index with Brick

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,8 +1,19 @@
 class UrlsController < ApplicationController
 
   def index
-    @urls = Url.all
+    @urls = Url.all.reverse
     @url = Url.new
+  end
+
+  def create
+    @url = Url.new(params[:url])
+
+    if @url.save
+      @url = Url.new
+      @urls = Url.all
+      render action: "index"
+    end
+
   end
 
 end
